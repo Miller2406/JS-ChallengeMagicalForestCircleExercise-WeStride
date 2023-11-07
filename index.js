af8d5813b8b76ee5e8fd53d7ba75a57545497f07
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-07 17:43:02 +0700

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+const div1 = document.querySelector('#container')
+
+console.log(div1);
+
+div1.style.textAlign = 'center'
+
+const treeImg = document.querySelector('img')
+console.log(treeImg);
+
+treeImg.style.borderRadius = '50%';
+treeImg.style.width = '150px';
+
